67a2f6a266a4675c84f3ab6c8eaaeb7540659eed
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-03-01 02:02:47 +0530

    Deployment fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import ShopContextProvider from './context/ShopContext.jsx';
 import './index.css';
-import ShopContextProvider from './context/shopContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
